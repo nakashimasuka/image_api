ece56ee198175b9d787fadcc94ae7554de5900df
DELALAY sofian <sofian.delalay@epitech.eu>
2020-02-16 15:51:21 +0100

    [+] add route '/'

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Try: '/refresh', '/get', /get/{index}', '/get/{index}/image'"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=30)
