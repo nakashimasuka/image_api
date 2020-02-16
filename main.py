3fb5f1f5912de89102c4928c704a1bfa01e0f3fd
DELALAY sofian <sofian.delalay@epitech.eu>
2020-02-16 16:18:17 +0100

    [+] '/get/{index}' done and '/get/{index}/image' almost done

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.secret_key = '2d9-E2.(f&é,P$p@fpb+zSU03êû9_'
 
 @app.route('/')
 def home():
-    return "Try: '/refresh', '/get', /get/{index}', '/get/{index}/image'"
+    return "Try: '/refresh', '/get', /get/{index}', '/get/{index}/image'. If there is no csv file, try to access '/refresh'"
 
 @app.route('/refresh/')
 def refresh():
@@ -45,7 +45,37 @@ def get():
     except OSError:
         return "An error occured"
 
+@app.route('/get/<int:num>/')
+def get_index(num):
+    try:
+        with open('file.csv') as f:
+            reader = csv.reader(f)
+            line = 0
+            for row in reader:
+                if line != 0:
+                    if line == num:
+                       return "{}".format(row)
+                line += 1
+            return "Error: try '/get' to see the index valable"
+    except OSError:
+        return "An error occured"
 
-
+@app.route('/get/<int:num>/image/')
+def get_image(num):
+    try:
+        with open('file.csv') as f:
+            reader = csv.DictReader(f)
+            line = 0
+            imgName = ''
+            for row in reader:
+                if line == num - 1:
+                    imgName = (row['filename'])
+                line += 1
+            if imgName == '':
+                return "Index not found. Try '/get'"
+            return send_from_directory(app.static_folder, './images/' + imgName, mimetype='image/jpg')
+    except OSError:
+        return "An error occured"
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
