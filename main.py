026c8a4fe8b08cab167f79c9f62eff9c20ef6414
DELALAY sofian <sofian.delalay@epitech.eu>
2020-02-16 15:53:46 +0100

    [*] change port 30 -> 3000; [-] main.py~

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import csv
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = '2d9-E2.(f&é,P$p@fpb+zSU03êû9_'
 
 @app.route('/')
 def index():
     return "Try: '/refresh', '/get', /get/{index}', '/get/{index}/image'"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=30)
+    app.run(host='0.0.0.0', port=3000)
